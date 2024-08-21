58df49857ed8c7d038e342621ff407563c65c569
Bilalpbhub004 <167087454+Bilalpbhub004@users.noreply.github.com>
2024-08-21 17:06:21 +0500

    Blissfuldayz

---
+++ Blissfuldayz/assets/js/carousel.js
@@ -0,0 +1,28 @@
+
+
+var owlCarousel = $('#owl-carousel');
+owlCarousel.owlCarousel({
+    loop: true,
+    margin: 30,
+    dots: false,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        320: {
+            items: 1
+        },
+        576: {
+            items: 3
+        },
+        768: {
+            items: 4
+        },
+
+
+    }
+});
